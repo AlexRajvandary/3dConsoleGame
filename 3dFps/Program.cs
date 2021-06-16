cc902d19e711802a994a7585f1739c7d60c21d58
AlexRajvandary <stark.11raj@gmail.com>
2021-06-16 13:35:07 +0300

    First commit

---
+++ 3dFps/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _3dFps
+{
+    class Program
+    {
+
+        private const int ScreenWidth = 100;
+        private const int ScreenHeight = 50;
+
+        private static readonly char[] Screen = new char[ScreenWidth * ScreenHeight];
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(ScreenHeight,ScreenWidth);
+            Console.SetBufferSize(ScreenHeight, ScreenWidth);
+            Console.CursorVisible = false;
+
+            char ch = ' ';
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ch = Console.ReadKey().KeyChar;
+                }
+
+                Array.Fill(Screen, ch);
+
+                Console.SetCursorPosition(0, 0);
+                Console.Write(Screen);
+            }
+        }
+    }
+}
